a6ca325c1dd74ab884765cc53a72513a10b6d253
hcs4125 <hcs4125@gmail.com>
2021-08-05 23:29:15 +0900

    login 구현

---
+++ app.js
@@ -0,0 +1,28 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USER_NAME_KEY = "username";
+
+function onLoginSubmit(info) {
+  info.preventDefault(); // 새로 고침 방지
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USER_NAME_KEY, username);
+  paintGreeting(username);
+}
+
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USER_NAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
