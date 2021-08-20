63ca8592cd2410a567aa22de42a82b916f92d6e1
hcs4125 <hcs4125@gmail.com>
2021-08-20 23:35:21 +0900

    useEffect 활용

--- shop/src/component/Detail.js
+++ shop/src/component/Detail.js
@@ -1,6 +1,6 @@
 /* eslint-disable eqeqeq */
 /* eslint-disable jsx-a11y/alt-text */
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
 import "../Detail.scss";
@@ -14,6 +14,18 @@ let Title = styled.h4`
 `;
 
 export function Detail(props) {
+  let [alert, setAlert] = useState(true);
+  let [value, setValue] = useState("");
+  //useEffect는 두번째 인자가 없으면 컴포넌트가 업데이트 될 때마다 실행된다. 그래서 두번째 인자에 해당 state를 넣어서 반복실행을 막아주면 좋다
+  //만약 두번째 인자로 빈 배열 [] 을 전달해주면 해당 useEffect는 해당 컴포넌트가 실행됨과 동시에 더이상 실행되지 않는다
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [alert]);
   let { id } = useParams();
   let index = parseInt(id);
   let history = useHistory();
@@ -26,9 +38,17 @@ export function Detail(props) {
       <Box>
         <Title color="blue">Detail</Title>
       </Box>
-      <div className="soldOut-yellow">
-        <p>재고가 얼마 남지 않았습니다.</p>
-      </div>
+      {value}
+      <input
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      {alert === true ? (
+        <div className="soldOut">
+          <p>곧 매진!</p>
+        </div>
+      ) : null}
       <div className="row">
         <div className="col-md-6">
           <img
@@ -41,7 +61,7 @@ export function Detail(props) {
         <div className="col-md-6 mt-4">
           <h4 className="pt-5">{findItem.title}</h4>
           <p>{findItem.content}</p>
-          <p>{findItem.price}</p>
+          <p>{findItem.price}원</p>
           <button className="btn btn-danger">주문하기</button>
           <button
             className="btn btn-danger"
--- shop/src/component/Item.js
+++ shop/src/component/Item.js
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import Image from "../images";
+import "../Item.scss";
+import { Link } from "react-router-dom";
 
 export function Item(props) {
   let itemList = [...props.shoes];
@@ -11,7 +13,9 @@ export function Item(props) {
     return (
       <div className="col-md-4" key={index}>
         <img src={imageList[index]} width="100%" />
-        <h4>{props.shoes[index].title}</h4>
+        <Link className="itemName" to={"/detail/" + index}>
+          {props.shoes[index].title}
+        </Link>
         <p>
           {props.shoes[index].content} & {props.shoes[index].price}
         </p>
