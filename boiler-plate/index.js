5ff86d8aa1aff40c25573cfb5444cf4652d11713
hcs4125 <hcs4125@gmail.com>
2021-09-08 15:16:09 +0900

    nodemon 설치 및 개인정보 보호용 config 생성

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 9000;
+const { User } = require("./models/user");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const config = require("./config/key");
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://brido:<비밀번호>@boilderplate.qvziz.mongodb.net/brido?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("hi mongoDB"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
