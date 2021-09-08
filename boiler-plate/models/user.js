949ab2c00e4dfac73ea047ad18814d134ae8c7bd
hcs4125 <hcs4125@gmail.com>
2021-09-08 19:36:53 +0900

    bcrypt 사용해서 비밀번호 암호화(salt,hash)

--- boiler-plate/models/user.js
+++ boiler-plate/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bycrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -25,6 +27,26 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    //비밀번호를 암호화 시키기
+    bycrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) {
+        console.log(err);
+        return next(err);
+      }
+      bycrypt.hash(user.password, salt, function (err, hash) {
+        if (err) {
+          console.log(err);
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  }
 });
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
