27d91283e50cceb6b8666bceb3a0bb8084fd0555
hcs4125 <hcs4125@gmail.com>
2021-09-08 11:35:50 +0900

    mongoDB 연결을 위한 mongoose 설치 및 사용

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 9000;
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://brido:hcshcs@boilderplate.qvziz.mongodb.net/brido?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("hi mongoDB"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
