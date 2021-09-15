447f15b6b1148aafcc069320014fceeb38ce22d4
hcs4125 <hcs4125@gmail.com>
2021-09-16 00:12:14 +0900

    Layout 설정

--- boiler-plate/client/src/components/views/LandingPage/LandingPage.js
+++ boiler-plate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,13 +1,55 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { Layout, Menu, Breadcrumb } from "antd";
 
-function LandingPage() {
+function LandingPage(props) {
+  const { Header, Content, Footer } = Layout;
+
   useEffect(() => {
     axios.get("/api/hello").then((res) => {
       console.log(res.data);
     });
   });
-  return <div>LandingPage</div>;
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃 실패");
+      }
+    });
+  };
+  return (
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">로그인</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: "0 50px", marginTop: 64 }}
+      >
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          className="site-layout-background"
+          style={{ padding: 24, minHeight: 380 }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
 }
 
 export default LandingPage;
--- boiler-plate/client/src/components/views/Navbar/Navbar.js
+++ boiler-plate/client/src/components/views/Navbar/Navbar.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    
+  );
 }
 
 export default Navbar;
