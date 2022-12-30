8dcd65c4b270c1f795648f11cb3e066ddb4dcb75
brido4125 <hcs4125@gmail.com>
2022-12-30 20:16:09 +0900

    RN setting

---
+++ sendwish/sendwish/sendwishApp.swift
@@ -0,0 +1,17 @@
+//
+//  sendwishApp.swift
+//  sendwish
+//
+//  Created by 홍창섭 on 2022/12/30.
+//
+
+import SwiftUI
+
+@main
+struct sendwishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
